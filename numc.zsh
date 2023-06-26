f0e45b29599a09c6a12a21de5dc1425be8e35684
Vaibhav Kaushal <vaibhav-kaushal@users.noreply.github.com>
2023-06-26 23:03:27 +0530

    Update numc.zsh

--- numc.zsh
+++ numc.zsh
@@ -1,33 +1,31 @@
 #!/usr/bin/env zsh
 
-function numc() {
-  case $1 in
+case $1 in
     d2o)
-      printf "%o" ${2}
-      ;;
+        printf "%o" ${2}
+    ;;
     o2d)
-      printf $(([##10]8#${2}))
-      ;;
+        printf $(([##10]8#${2}))
+    ;;
     h2d)
-      print $((16#${2}))
-      ;;
+        print $((16#${2}))
+    ;;
     d2h)
-      printf "%x" ${2}
-      ;;
+        printf "%x" ${2}
+    ;;
     d2H)
-      printf "%X" ${2}
-      ;;
+        printf "%X" ${2}
+    ;;
     d2B36)
-      print $(([##36]${2}))
-      ;;
+        print $(([##36]${2}))
+    ;;
     d2b36)
-      print $(string lower $(([##36]${2})))
-      ;;
+        print $(string lower $(([##36]${2})))
+    ;;
     b362d)
-      print $(([##10]36#${2}))
-      ;;
+        print $(([##10]36#${2}))
+    ;;
     help|*)
-      source ${functions_source[$0]:a:h}/${0}_help.zsh $0
-      ;;
-  esac
-}
+        source ${0:A:h}/help.zsh numc
+    ;;
+esac
